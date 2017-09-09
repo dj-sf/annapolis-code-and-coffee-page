34367e00c7edfa60dde2eb786993325ac4c794be
Jim Stricker <jmstricker93@gmail.com>
2017-09-09 14:48:05 -0400

    initial commit

---
+++ public_html/index.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta http-equiv="cache-control" content="max-age=0" />
+  <meta http-equiv="cache-control" content="no-cache" />
+  <meta http-equiv="expires" content="0" />
+  <meta http-equiv="pragma" content="no-cache" />
+  <title>Annapolis Code and Coffee</title>
+  <style>
+
+
+  html
+  {
+    font-size: 62.5%;
+  }
+
+  body
+  {
+    margin: 0;
+    padding: 0;
+    font-size: 1.6rem;
+    color: #353535;
+    background: linear-gradient(to bottom, #ccc, #fff 30%);
+    background-repeat: no-repeat;
+  }
+
+  h1,h3
+  {
+    font-family: 'Montserrat', sans-serif;
+    text-align: center;
+    margin: 0;
+    padding: 0;
+    text-transform: uppercase;
+  }
+
+
+  h1
+  {
+    margin: 20rem 0 1rem;
+    font-size: 3rem;
+  }
+
+  h3
+  {
+    font-weight: normal;
+    font-size: 1.4rem;
+  }
+  </style>
+</head>
+<body>
+  <h1>Annapolis Code and Coffee</h1>
+  <p><p><p>
+    Thanks for stopping by!  We had our inaugural meetup on 9.9. 2017, and have many exciting things planned!  Check back soon for details!<p>
+  
+  <p>
+    In the meantime, check out our <a href="https://docs.google.com/document/d/19WnISeu6lfILK1n-zAfogFYYrep4pwXFQ6anLCkb5Dw/edit?usp=sharing">meeting notes</a> from the most recent meeting!
+    
+    <p>We just put this site up (LITERALLY, like 5 minutes ago LOL), and would love your input on the content, and the coding!  <a href="https://docs.google.com/document/d/1E0NF6qW40o-oCBB768kYl1LXaVg0pnlXKwHvZWANkgg/edit?usp=sharing">Click here</a> to let us know what you would find useful, and what you'd like to work on for the site!
+ 
+  <p>
+  <p>
+    For more information, visit our meetup site at: <a href="https://www.meetup.com/Annapolis-Code-Coffee/">https://www.meetup.com/Annapolis-Code-Coffee/</a>.  You can also email us at <strong>info@annapoliscodeandcoffee.com</strong>. 
+    
+ 
+  <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+</body>
+</html>
